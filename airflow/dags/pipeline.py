18d86927a0d53b584530d0a47dd72face6fdc6fc
Akalika Callistus Anthony <108539957+sutsillac@users.noreply.github.com>
2023-07-29 18:26:37 +0100

    Thisis my first supervised change

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -12,3 +12,5 @@ python sample.py \
   --db=covid_db \
   --table_name=covid_data \
   --url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv"
+Testing testing 1 and 2
+lets collaborate
